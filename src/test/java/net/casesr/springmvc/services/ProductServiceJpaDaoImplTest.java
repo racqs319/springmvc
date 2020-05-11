2d2a7d79c7cb1e7240d015f8888f849f9d4b9fba
Racquel Cases <racquel.cases@hotmail.com>
2020-05-11 11:54:20 +0800

    Add integration test for product service

---
+++ src/test/java/net/casesr/springmvc/services/ProductServiceJpaDaoImplTest.java
@@ -0,0 +1,37 @@
+package net.casesr.springmvc.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import net.casesr.springmvc.config.JpaIntegrationConfig;
+import net.casesr.springmvc.domain.Product;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {JpaIntegrationConfig.class})
+@ActiveProfiles("jpadao")
+public class ProductServiceJpaDaoImplTest {
+	
+	private ProductService productService;
+
+	@Autowired
+	public void setProductService(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@Test
+	public void testListMethod() throws Exception {
+		@SuppressWarnings("unchecked")
+		List<Product> products = (List<Product>) productService.listAll();
+		
+		assertEquals(5, products.size());
+	}
+
+}
